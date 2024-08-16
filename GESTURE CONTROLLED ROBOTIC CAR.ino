1731347c7964a767d3b7ba905ffd497817127880
Shivam Patel <100112564+ShivamPatel010902@users.noreply.github.com>
2024-08-16 14:28:06 +0530

    Add files via upload

---
+++ GESTURE CONTROLLED ROBOTIC CAR.ino
@@ -0,0 +1,89 @@
+#include <SPI.h>
+      //SPI library for communicate with the nRF24L01+ 
+#include "RF24.h"     //The main library of the nRF24L01+ 
+//Define enable pins of the Motors 
+const int enbA = 2; 
+const int enbB = 7; 
+//Define control pins of the Motors 
+//If the motors rotate in the opposite direction, you can change the positions of the following pin 
+numbers 
+const int IN1 = 3;    //Right Motor (-) 
+const int IN2 = 4;    //Right Motor (+) 
+const int IN3 = 5;    //Left Motor (+) 
+const int IN4 = 6;    //Right Motor (-) 
+//Define variable for the motors speeds 
+//I have defined a variable for each of the two motors  
+//This way you can synchronize the rotation speed difference between the two motors 
+int RightSpd = 130; 
+int LeftSpd = 150; 
+//Define packet for the direction (X axis and Y axis) 
+int data[2]; 
+//Define object from RF24 library - 9 and 10 are a digital pin numbers to which signals CE and CSN 
+are connected 
+31 
+RF24 radio(8,9); 
+//Create a pipe addresses for the communicate 
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+void setup(){ 
+//Define the motor pins as OUTPUT 
+pinMode(enbA, OUTPUT); 
+pinMode(enbB, OUTPUT); 
+pinMode(IN1, OUTPUT); 
+pinMode(IN2, OUTPUT); 
+pinMode(IN3, OUTPUT); 
+pinMode(IN4, OUTPUT); 
+Serial.begin(9600); 
+radio.begin();                 
+   //Start the nRF24 communicate             
+radio.openReadingPipe(1, pipe);   //Sets the address of the transmitter to which the program will 
+receive data. 
+radio.startListening();              
+} 
+void loop(){ 
+if (radio.available()){ 
+radio.read(data, sizeof(data)); 
+if(data[0] > 380){ 
+//forward             
+analogWrite(enbA, RightSpd); 
+analogWrite(enbB, LeftSpd); 
+32 
+digitalWrite(IN1, HIGH); 
+digitalWrite(IN2, LOW); 
+digitalWrite(IN3, HIGH); 
+digitalWrite(IN4, LOW); 
+} 
+if(data[0] < 310){ 
+//backward               
+analogWrite(enbA, RightSpd); 
+analogWrite(enbB, LeftSpd); 
+digitalWrite(IN1, LOW); 
+digitalWrite(IN2, HIGH); 
+digitalWrite(IN3, LOW); 
+digitalWrite(IN4, HIGH); 
+} 
+} 
+if(data[1] > 180){ 
+//left 
+analogWrite(enbA, RightSpd); 
+analogWrite(enbB, LeftSpd); 
+digitalWrite(IN1, HIGH); 
+digitalWrite(IN2, LOW); 
+digitalWrite(IN3, LOW); 
+digitalWrite(IN4, HIGH); 
+if(data[1] < 110){ 
+//right 
+33 
+analogWrite(enbA, RightSpd); 
+analogWrite(enbB, LeftSpd); 
+digitalWrite(IN1, LOW); 
+digitalWrite(IN2, HIGH); 
+digitalWrite(IN3, HIGH); 
+digitalWrite(IN4, LOW); 
+} 
+if(data[0] > 330 && data[0] < 360 && data[1] > 130 && data[1] < 160){ 
+//stop car 
+analogWrite(enbA, 0); 
+analogWrite(enbB, 0); 
+} 
+} 
+} 
